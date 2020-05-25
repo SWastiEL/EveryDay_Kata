224eccc91a354de10a0ad0484073d5812a32a44e
SWastiEL <slowkingart@gmail.com>
2020-05-25 12:44:14 +0700

    Start project

---
+++ CodeWars/NP_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
